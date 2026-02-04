afa024d675ecc664a976ffa226d6fae7fc4f3b1b
Curro Portillo <211curroportillo@gmail.com>
2026-02-04 20:49:21 +0100

    perseguir basico

--- src/juego/movimientos/Perseguir.java
+++ src/juego/movimientos/Perseguir.java
@@ -6,8 +6,12 @@ public class Perseguir implements Movimiento{
 
     @Override
     public Posicion mover(Posicion actual, Posicion objetivo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mover'");
+        int vx, vy;
+        do{
+            vx = (int)(Math.random()*3-1);
+            vy = (int)(Math.random()*3-1);
+        }while(vy == 0 && vx == 0);
+        return new Posicion(actual.getX()+vx, actual.getY()+vy);
     }
     
 }
